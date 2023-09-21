72dc5f0804cb1d3f3bd13e48bc465b863d5ecd66
Guiii603 <136653822+Guiii603@users.noreply.github.com>
2023-09-21 19:37:36 -0300

    Add files via upload

--- Exc_1.cpp
+++ Exc_1.cpp
@@ -21,7 +21,7 @@ int main (){
 	char nome[20] = "Guilherme Oechsler";
 	char nome2[20] = "Jose Mateus"; 
 	int first = 0;
-	int *aux, *aux2;
+	int aux, aux2;
 	
 	 printf("Ordem alfabetica pelo nome");
 	 if(nome[first]>nome2[first]){
@@ -30,11 +30,11 @@ int main (){
 	 	printf("\n\t%s\n\t%s", nome, nome2);
 	 }
 
-	split(nome, aux);
-	split(nome2, aux2);
+	split(nome, &aux);
+	split(nome2, &aux2);
 
 	 printf("\n\nOrdem alfabetica pelo sobrenome");
-	 if(nome[*aux]>nome2[*aux2]){
+	 if(nome[aux]>nome2[aux2]){
 	 	printf("\n\t%s\n\t%s", nome2, nome);
 	 }else{
 	 	printf("\n\t%s\n\t%s", nome, nome2);
