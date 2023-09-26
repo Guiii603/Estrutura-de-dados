33949f6e60ec9cb7005b90c0485b735fecd60837
Guiii603 <136653822+Guiii603@users.noreply.github.com>
2023-09-26 00:11:29 -0300

    Add files via upload

--- Exc_1.cpp
+++ Exc_1.cpp
@@ -16,22 +16,53 @@ int split(char a[], int *aux){
 	}
 }
 
-int comparacao_de_letras(char a[], char b[], int i){
-	i = -1;
+int comparacao_de_letras(char a[], char b[], int *i){
+	*i = -1;
 	do{
-	i++;
-	}while(a[i] == b[i]);		
-	
+	*i++;
+	}while(a[*i] == b[*i]);	
 } 
 
+	void separa_nome(char a[]) {
+    int i = 0;
+    while(a[i] != ' '){
+        printf("%c", a[i]);
+        i++;
+    }
+}
+
+int valor_posicao_nome(char a[], char *s[]){
+	int j = 0;
+	int i = 0;
+	do{
+		*s[j+1] = a[i];
+		i++;
+		
+	}while(a[i] != ' ' && i < 20);
+
+	
+}
+
+
+int busca_posicao_letra_diferente(char a[], char b[]){
+	
+	for(int i=0;i<20;i++){
+		if(a[i] != b[i]){
+			return i;
+		}
+	}
+	return -1;
+}
+	
 
 int main (){
-	
+
 	char nome[20] = "Guilherme Oechsler";
-	char nome2[20] = "jose matheus"; 
+	char nome2[20] = "Keyla Rubia"; 
+	char string[20];
 	int letra;
 	int aux, aux2;
-	
+
 	 printf("Ordem alfabetica pelo nome");
 	 letra = -1;
 	do{
