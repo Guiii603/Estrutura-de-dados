2817f5e2004397aa04f0e9c7c51926d79eaeeeb1
Guiii603 <136653822+Guiii603@users.noreply.github.com>
2023-10-09 20:39:16 -0300

    Add files via upload

--- Exc_1.cpp
+++ Exc_1.cpp
@@ -1,5 +1,5 @@
-/*Exe_01.Faça umalgoritmo utilizando o minimo de bibliotecas possivel,
- que leia dois nomes completos, contendoapenas um nomee um sobrenome. 
+/*Exe_01.Faça um algoritmo utilizando o minimo de bibliotecas possivel,
+ que leia dois nomes completos, contendo apenas um nome e um sobrenome. 
  O programa deve exibir os nomes completosda seguinte forma:
 
 a)Ordenados alfabeticamente pelo nome.
@@ -42,40 +42,52 @@ char separa_sobrenome(char a[], char b[]){
 	 } while(a[i] != ' ' && a[i] != '\0');
 }
 
-int compara_ordem_alfabetica(){
+int compara_ordem_alfabetica(char nome[], char nome2[], int letra, char retorno[], char retorno2[]){
+	if(nome[letra]>nome2[letra]){
+	 	printf("\n\t%s\n\t%s", retorno2, retorno);
+	 }else{
+	 	printf("\n\t%s\n\t%s", retorno, retorno2);
+	 }
+}
+
+int transforma_maiusculo(char nome[],char nome2[]){
 	
+	for(int i=0;i<50;i++){
+		if(nome[i]>=97){
+			nome[i]-=32;
+		}
+	}
+		for(int i=0;i<50;i++){
+		if(nome2[i]>=97){
+			nome2[i]-=32;
+		}
+	}
 }
 	
 int main(){
 
-	char nome[50] = "guilherme oechsler", nome2[50] = "keyla rubia";
+	char nome[50] = "Guilherme Oechsler", nome2[50] = "Jose Matheus";
 	char string[50], sobrenome[50], string2[50], sobrenome2[50];
-	int letra, letra2, aux, aux2;
+	int letra;
 	
+	transforma_maiusculo(nome, nome2);
+
 	separa_nome(nome, string);
 	separa_nome(nome2, string2);
 	
-	 printf("Ordem alfabetica pelo nome");
+	 printf("\nOrdem alfabetica pelo nome");
 	 
 	letra = busca_posicao_letra_diferente(nome, nome2);
-	
-	 if(string[letra]>string2[letra]){
-	 	printf("\n\t%s\n\t%s", nome2, nome);
-	 }else{
-	 	printf("\n\t%s\n\t%s", nome, nome2);
-	 }
+
+	 compara_ordem_alfabetica(string, string2, letra, nome, nome2);
 
 	 printf("\n\nOrdem alfabetica pelo sobrenome"); 
 	 
 	separa_sobrenome(string, sobrenome);
 	separa_sobrenome(string2, sobrenome2);
 	
-	letra2 = busca_posicao_letra_diferente(sobrenome, sobrenome2);
+	letra = busca_posicao_letra_diferente(sobrenome, sobrenome2);
 
-	 if(sobrenome[letra2]>sobrenome2[letra2]){
-	 	printf("\n\t%s\n\t%s", nome2, nome);
-	 }else{
-	 	printf("\n\t%s\n\t%s", nome, nome2);
-	 }
-	 
+	compara_ordem_alfabetica(sobrenome, sobrenome2, letra, nome, nome2);
+	
 }
